d14ce96d19864349ebb58d165d0fa665564d7a72
Nzai <nzai@localhost.localdomain>
2022-05-13 16:39:20 +0300

    trees

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //register
 
@@ -21,9 +22,36 @@ router.post('/register', async(req, res) => {
 });
 
 //login
-router.post("/login") ,async (req, res) => {
+router.post("/login" , async (req, res) => {
+    try{
+        const user = await User .findOne({username: req.body.username});
+        !user && res.status(404).json("User not found");
+        
+
+        const hashedpass = CryptoJS.AES.decrypt(user.password, process.env.SEC_PASS);
+       
     
-}
+        const  originalPassword = hashedpass.toString(CryptoJS.enc.Utf8);
+        
+        originalPassword !== req.body.password && res.status(401).json("Password is incorrect");
+
+
+        const token = jwt.sign(
+            {
+            id:user._id, 
+            isAdmin:user.isAdmin,
+           },
+            process.env.JWT_SEC, 
+        {expiresIn: "3d"});
+        
+
+        const {password, ...others} = user._doc;
+        res.status(200).json({...others, token});
+    }catch (err){
+        res.status(500).json(err);
+    }
+   
+    });
 
 
 
