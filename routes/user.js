defac0b695e6a03b69166427f09983daee27897d
Nzai <nzaih18@gmail.com>
2022-05-15 15:04:24 +0300

    fixed CRUD ERRORS

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,33 @@
-const { verifyToken, verifyTokenAndAuth } = require('./verfyToken');
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
 
+} = require("./verifyToken");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-//update user
-router.put("/:id", verifyTokenAndAuth , async (req, res) => {
-   if(req.body.password){
-    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SEC_PASS).toString();
-   }
-    try{
-        const updatedUser = await User.findByIdAndUpdate(req.params.id , {
-            $set:req.body,
-        }
-        , {new: true});
-         console.log(updatedUser)
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SEC_PASS
+    ).toString();
+  }
 
-        res.status(200).json(updatedUser);
-
-    }catch(err){
-        res.status(500).json(err);
-    }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-module.exports = router; 
+module.exports = router
