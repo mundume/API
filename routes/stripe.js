0df8f244ca7a506ef1a63b71292fb7b379be3ad0
Nzai <nzaih18@gmail.com>
2022-05-26 17:55:33 +0300

    stripe config

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,18 +1,18 @@
-const { Router } = require("express");
 
 const router = require("express").Router();
-const stripe = require ("stripe")(process.env.STRIPE_KEY)
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
 
 
 //PAYMENT
 router.post("/payment",  (req, res) => {
     stripe.charges.create({
-        source:req.body.token.id,
+        source:req.body.tokenId,
         amount: req.body.amount,
         currency:"usd"
     }, (stripeError, stripeRes)=>{
         if(stripeError){
-            res.status(500).json(err)
+            res.status(500).json(stripeError)
         }else{
             res.status(200).json(stripeRes)
         }
